5c0680c62640c8ca2b09192ad654021cf014b8e0
Alex Zhukovich <zhukovich.a@gmail.com>
2019-03-18 23:33:21 +0100

    Updated Team and Employee data entities; Updated layouts for team and employee items; Updated Api#BASE_URL value

--- app/src/main/java/com/alexzh/company/contacts/data/Employee.kt
+++ app/src/main/java/com/alexzh/company/contacts/data/Employee.kt
@@ -8,6 +8,8 @@ import android.arch.persistence.room.PrimaryKey
 data class Employee(
         @ColumnInfo(name = "team_id") val teamId: Long,
         @ColumnInfo(name = "name") val name: String,
+        @ColumnInfo(name = "photo") val photo: String,
+        @ColumnInfo(name = "position") val position: String,
         @ColumnInfo(name = "phone") val phone: String) {
 
     @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "employee_id") var id: Long = 0
--- app/src/main/java/com/alexzh/company/contacts/employees/EmployeeViewHolder.kt
+++ app/src/main/java/com/alexzh/company/contacts/employees/EmployeeViewHolder.kt
@@ -3,6 +3,8 @@ package com.alexzh.company.contacts.employees
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import com.alexzh.company.contacts.data.Employee
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import kotlinx.android.synthetic.main.item_employee.view.*
 
 class EmployeeViewHolder(view: View, private val itemClick: (Employee) -> Unit): RecyclerView.ViewHolder(view) {
@@ -11,7 +13,13 @@ class EmployeeViewHolder(view: View, private val itemClick: (Employee) -> Unit):
         with(employee) {
             itemView.employeeName.text = name
             itemView.employeePhone.text = phone
+            itemView.employeePosition.text = position
             itemView.setOnClickListener { itemClick(this) }
+
+            Glide.with(itemView.context)
+                    .load(photo)
+                    .apply(RequestOptions.circleCropTransform())
+                    .into(itemView.employeePhoto)
         }
     }
 }
